2e4717d1ba637d153cfc63c27588a34163e7ed4a
Fanantenana <110564296+fanantenana1@users.noreply.github.com>
2026-08-12 11:36:23 -0400

    Change Cloudinary API credentials
    Updated Cloudinary configuration with new credentials.

--- cloudinary_config.py
+++ cloudinary_config.py
@@ -2,9 +2,9 @@ import cloudinary
 import cloudinary.uploader
 
 cloudinary.config(
-  cloud_name="dold0f7np",
-  api_key="831495674953395",
-  api_secret="JseXluM2-m4f_Hw9XdCqZ2RJliQ",
+  cloud_name="nam",
+  api_key="******",
+  api_secret="******",
   secure=True
 )
 
